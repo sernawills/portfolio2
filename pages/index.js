99a4d1e3ab61ede3f1bcaa591ccdba3b29f375e8
sernawills <luciana.sernawills@student.hpi.uni-potsdam.de>
2022-02-21 12:56:36 +0100

    fix indenting

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,20 @@
 import Link from 'next/link';
 
-    function Home() {
-        return (
-            <div className="home" >
-                <div className="headerContainer">
-                    <h1> Hi, I am <Link href="/about" id="aboutMeLink"><a>Luciana</a></Link> </h1>
-                    <p> UX designer based in Berlin </p>
-                    <Link href="/portfolio">
-                        <button> SEE PORTFOLIO </button>
-                    </Link>
-                    <Link href="/Test">
-                        <button> Test it here! </button>
-                    </Link>
-                </div>
+function Home() {
+    return (
+        <div className="home" >
+            <div className="headerContainer">
+                <h1> Hi, I am <Link href="/about" id="aboutMeLink"><a>Luciana</a></Link> </h1>
+                <p> UX designer based in Berlin </p>
+                <Link href="/portfolio">
+                    <button> SEE PORTFOLIO </button>
+                </Link>
+                <Link href="/Test">
+                    <button> Test it here! </button>
+                </Link>
             </div>
-        )
-    }
-    
-    export default Home;
-    
+        </div>
+    )
+}
+
+export default Home;
